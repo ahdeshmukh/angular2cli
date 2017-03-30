87eec8c250f252887c588a200e94da0eb369c1c3
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2017-03-30 14:35:43 -0400

    new files and updates to some other files to print from header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UsersComponent } from './components/users/users.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
