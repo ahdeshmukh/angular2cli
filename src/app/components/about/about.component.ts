332a672833881d854955dfed6f9fb447dadb1017
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2017-04-05 17:18:52 -0400

    updates to about page

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { UtilityService } from '../../services/utility/utility.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
+  providers: [UtilityService]
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  imagesPath: string;
+  logoImagesPath: string;
+
+  constructor(private utilityService: UtilityService) { }
 
   ngOnInit() {
+    this.imagesPath = this.utilityService.getImagesBasePath();
+    this.logoImagesPath = this.imagesPath + '/logos/';
   }
 
 }
