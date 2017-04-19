e29fe721827bbc1cc2a4357ca28d2a1567be7c7e
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2017-04-19 14:17:47 -0400

    removing textboxes from login modal. work in progress

--- src/app/components/authentication/authentication.component.ts
+++ src/app/components/authentication/authentication.component.ts
@@ -1,23 +1,37 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ModalDirective } from 'ng2-bootstrap/modal';
+import { UserService } from '../../services/user/user.service';
 
 @Component({
   selector: 'app-authentication',
   templateUrl: './authentication.component.html',
-  styleUrls: ['./authentication.component.css']
+  styleUrls: ['./authentication.component.css'],
+  providers: [UserService]
 })
 export class AuthenticationComponent implements OnInit {
 
   @ViewChild('loginModal') public loginModal:ModalDirective;
   
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
   }
 
   login(formValue: any) {
     let currentUser: any;
-    currentUser = {"firstName": formValue.firstName, "lastName": formValue.lastName, "role": formValue.userRole};
+    let firstName: string, lastName: string;
+
+    if(formValue.userRole === 'admin') {
+      firstName = 'Admin';
+      lastName = 'User';
+    } else {
+      let user: any;
+      this.userService.getUsers(1)
+      .subscribe(respUser => user = respUser);
+    }
+    
+    currentUser = {"firstName": "Admin", "lastName": "User", "role": formValue.userRole};
+
     localStorage.setItem('currentUser', currentUser);
     //this.hideLoginModal();
   }
