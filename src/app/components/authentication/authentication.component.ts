395bb15c46fafaff7d1d0ca47cc710809156cde2
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2017-04-22 20:42:36 -0400

    adding loader and hiding submit buttons while logging in

--- src/app/components/authentication/authentication.component.ts
+++ src/app/components/authentication/authentication.component.ts
@@ -2,25 +2,39 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { ModalDirective } from 'ng2-bootstrap/modal';
 import { UserService } from '../../services/user/user.service';
 import { AuthenticationService } from '../../services/authentication/authentication.service';
+import { UtilityService } from '../../services/utility/utility.service';
 
 @Component({
   selector: 'app-authentication',
   templateUrl: './authentication.component.html',
   styleUrls: ['./authentication.component.css'],
-  providers: [UserService, AuthenticationService]
+  providers: [UserService, AuthenticationService, UtilityService]
 })
 export class AuthenticationComponent implements OnInit {
 
   @ViewChild('loginModal') public loginModal:ModalDirective;
   @ViewChild('accountModal') public accountModal:ModalDirective;
+
+  isLoginInProgress:boolean = false;
+  imagesPath:string;
+  currentUser:any;
+
   
-  constructor(private userService: UserService, private authenticationService: AuthenticationService) { }
+  constructor(private userService: UserService, 
+              private authenticationService: AuthenticationService, 
+              private utilityService: UtilityService) { }
 
   ngOnInit() {
+    this.imagesPath = this.utilityService.getImagesBasePath();
   }
 
   login(formValue: any) {
-    this.authenticationService.login(formValue);
+    this.isLoginInProgress = true;
+    this.authenticationService.login(formValue)
+      .subscribe(value => {
+        this.isLoginInProgress = false;
+        this.currentUser = this.authenticationService.getCurrentLoggedInUser();
+      });
   }
 
   logout() {
@@ -29,6 +43,10 @@ export class AuthenticationComponent implements OnInit {
 
   isUserLoggedIn():boolean {
     return this.authenticationService.isUserLoggedIn();
+  }
+
+  getCurrentUser():any {
+    this.currentUser = this.authenticationService.getCurrentLoggedInUser();
   }
 
   showLoginModal():void {
--- src/app/services/authentication/authentication.service.ts
+++ src/app/services/authentication/authentication.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { UserService } from '../../services/user/user.service';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class AuthenticationService {
@@ -9,9 +10,9 @@ export class AuthenticationService {
   login(userCredentials: any) {
     
     let currentUser: any;
-    let firstName: string, lastName: string;
+    let firstName: string, lastName: string, email: string;
     
-    setTimeout(() => {
+    /*setTimeout(() => {
       if(userCredentials.userRole === 'admin') {
         firstName = 'Admin';
         lastName = 'User';
@@ -24,7 +25,33 @@ export class AuthenticationService {
       currentUser = {"firstName": "Admin", "lastName": "User", "role": userCredentials.userRole};
       
       localStorage.setItem('currentUser', currentUser);
-    }, 3000);
+    }, 3000);*/
+
+    let observable$ = new Observable(observer => {
+      setTimeout(() => {
+        if(userCredentials.userRole === 'admin') {
+          firstName = 'Admin';
+          lastName = 'User';
+          email = 'admin@example.com';
+        } else {
+          // TODO - make a call to randouser.me and just get 1 record back
+          firstName = 'Authenticated';
+          lastName = 'User';
+          email = 'auth@example.com';
+        }
+        
+        // TODO - create an object of type currentLoggedInUser which has firstName, lastName, email and role properties
+        currentUser = {"firstName": firstName, "lastName": lastName, "email": email, "role": userCredentials.userRole};
+        localStorage.setItem('currentUser', JSON.stringify(currentUser));
+        observer.next(this.isUserLoggedIn());
+      }, 3000)
+    });
+    observable$.subscribe(
+      value => value,
+      err => {},
+      () => console.log('this is the end')
+    );
+    return observable$;
 
   }
 
@@ -33,6 +60,7 @@ export class AuthenticationService {
   }
 
   isUserLoggedIn():boolean {
+    // TODO - check if currentUser object is of type currentLoggedInUser created in loin method
     let currentUser: any = localStorage.getItem('currentUser');
     if(currentUser && currentUser != "undefined" && currentUser != "null") {
       return true;
@@ -40,7 +68,7 @@ export class AuthenticationService {
     return false;
   }
 
-  getCurrentLoggedInuser() {
+  getCurrentLoggedInUser() {
     return localStorage.getItem('currentUser');
   }
 
