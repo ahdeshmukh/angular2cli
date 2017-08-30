d885a998411d957d44da271601d0b63f13b41b02
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2017-08-30 11:13:52 -0400

    updating angular - plus highlight.directive.spec.ts suddenly started to throw an error - so put a quick fix

--- src/app/directives/highlight.directive.spec.ts
+++ src/app/directives/highlight.directive.spec.ts
@@ -5,7 +5,7 @@ import { HighlightDirective } from './highlight.directive';
 
 describe('HighlightDirective', () => {
   it('should create an instance', () => {
-    const directive = new HighlightDirective();
+    const directive = new HighlightDirective(null);
     expect(directive).toBeTruthy();
   });
 });
